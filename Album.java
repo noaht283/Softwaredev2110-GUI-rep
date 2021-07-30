dafd5f51838cfc8c56a592b57f1902d44c1b1d3e
noaht283 <88215641+noaht283@users.noreply.github.com>
2021-07-30 19:34:00 -0400

    Create Album.java

---
+++ Album.java
@@ -0,0 +1,9 @@
+public class Album extends PhotographContainer {
+
+	public Album(String name) {
+		// Album class extends the abstract class PhotographContainer
+		// all methods will go to the abstract class
+		super(name);
+
+	}
+}
